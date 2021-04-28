3fb809f40ee87167e1710a12119a8e24719e8b95
tihoangyeudau <tuantrung2905@gmail.com>
2021-04-28 11:29:30 +0700

    update

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -9,13 +9,12 @@ HYPERION_LATEST_VERSION=$(curl -sL "$HYPERION_RELEASES_URL" | grep "tag_name" | 
 HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Ambilight-WiFi-$HYPERION_LATEST_VERSION-Linux-armv6l.deb
 
 # Download latest release
-echo '           Download Ambilight WiFi + rpi fan'
+echo 'Downloading Ambilight WiFi + rpi fan ........................'
 mkdir -p "$ROOTFS_DIR"/tmp
-curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/ambilight-wifi.deb
+curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/ambilightwifi.deb
 curl -sS -L --get https://github.com/tihoangyeudau/rpi-fan/releases/download/1.0.0/rpi-fan.tar.gz | tar --strip-components=0 -C ${ROOTFS_DIR}/usr/share/ rpi-fan -xz
 
 # Copy service file
-cp ambilightwifi.service ${ROOTFS_DIR}/etc/systemd/system/ambilightwifi.service
 cp rpi-fan.service ${ROOTFS_DIR}/etc/systemd/system/rpi-fan.service
 
 # Enable SPI and force HDMI output
@@ -26,12 +25,22 @@ sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/con
 sed -i "s/Raspbian/HyperBian/gI" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/www.facebook.com\/rainbowmusicled\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+
+# Custom motd
+rm "${ROOTFS_DIR}"/etc/motd
+rm "${ROOTFS_DIR}"/etc/update-motd.d/10-uname
+install -m 755 files/motd-hyperbian "${ROOTFS_DIR}"/etc/update-motd.d/10-hyperbian
+
+# Remove the "last login" information
+sed -i "s/^#PrintLastLog yes.*/PrintLastLog no/" ${ROOTFS_DIR}/etc/ssh/sshd_config
 
 on_chroot << EOF
-echo '           Install Ambilight WiFi'
-apt install /tmp/ambilight-wifi.deb
-rm -f /etc/systemd/system/ambilightwifid@.service
-echo '           Register Ambilight WiFi'
-systemctl -q enable ambilightwifi.service
-systemctl -q enable rpi-fan.service
+echo 'Installing Ambilight WiFi ........................'
+apt install /tmp/ambilightwifi.deb
+rm /tmp/ambilightwifi.deb
+mv /etc/systemd/system/ambilightwifid@.service /etc/systemd/system/ambilightwifi@.service
+echo 'Registering Ambilight WiFi ........................'
+systemctl -q enable ambilightwifi"@rml".service
+systemctl -q enable rpi-fan"@rml".service
 EOF
